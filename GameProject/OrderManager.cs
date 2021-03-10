3b987a382dc1f512f2b4167df85f108219a43453
buketk <61161607+buketk@users.noreply.github.com>
2021-03-10 17:01:21 +0300

    GameProject

---
+++ GameProject/OrderManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    
+    class OrderManager:IOrderService
+    {
+        CampaignManager _campaignManager;
+
+        public OrderManager(CampaignManager campaignManager)
+        {
+            _campaignManager = campaignManager;
+        }
+
+        public void Calculate()
+        {
+            Console.WriteLine("Toplam Tutar Hesaplandı.");
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1994 && gamer.FirstName == "BUKET" 
+                && gamer.LastName == "KARAÇAYLI" && gamer.IdentityNumber == 1234)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
